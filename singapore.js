539d79357b57aa59397aa495006a3845edf08cf9
Maythee Anegboonlap <null@llun.me>
2021-07-04 15:10:35 +0800

    Init

---
+++ singapore.js
@@ -0,0 +1,40 @@
+// @ts-check
+import DateFnsTz from 'date-fns-tz'
+
+const { format, utcToZonedTime } = DateFnsTz
+
+/**
+ *
+ * Get weather.gov.sg file time format from timestamp
+ *
+ * @param {number} timestamp
+ * @param {number} minutesFloor
+ * @returns {string}
+ */
+export function getFileNameTime(timestamp, minutesFloor) {
+  const timeZone = 'Asia/Singapore'
+  const subtractFloor = timestamp % (minutesFloor * 60 * 1000)
+  const date = new Date(timestamp - subtractFloor)
+  const zonedDate = utcToZonedTime(date, timeZone)
+  return format(zonedDate, 'yyyyMMddkkmmssSS', { timeZone })
+}
+
+/**
+ *
+ * Get weather.gov.sg url from timestamp
+ *
+ * @param {number} timestamp
+ * @param {50 | 240 | 480} areaSizeInKm
+ * @returns {string}
+ */
+export function getRainAreaUrlFromTimestamp(timestamp, areaSizeInKm) {
+  switch (areaSizeInKm) {
+    case 50:
+      return `http://www.weather.gov.sg/files/rainarea/50km/v2/dpsri_70km_${getFileNameTime(
+        timestamp,
+        5
+      )}dBR.dpsri.png`
+    default:
+      return ''
+  }
+}
---
+++ singapore.test.js
@@ -0,0 +1,28 @@
+// @ts-check
+import test from 'ava'
+import { getFileNameTime, getRainAreaUrlFromTimestamp } from './singapore.js'
+// http://www.weather.gov.sg/files/rainarea/50km/v2/dpsri_70km_2021070409500000dBR.dpsri.png
+// http://www.weather.gov.sg/files/rainarea/50km/v2/dpsri_70km_2021070409550000dBR.dpsri.png
+// http://www.weather.gov.sg/files/rainarea/50km/v2/dpsri_70km_2021070409250000dBR.dpsri.png
+
+test('#getFileNameTime return date from timestamp in Singapore timezone', (t) => {
+  t.is(getFileNameTime(1625369575346, 5), '2021070411300000')
+  t.is(getFileNameTime(1625383277705, 5), '2021070415200000')
+
+  t.is(getFileNameTime(1625369575346, 15), '2021070411300000')
+  t.is(getFileNameTime(1625383277705, 15), '2021070415150000')
+
+  t.is(getFileNameTime(1625369575346, 30), '2021070411300000')
+  t.is(getFileNameTime(1625383277705, 30), '2021070415000000')
+})
+
+test('#getRainAreaUrlFromTimestamp', (t) => {
+  t.is(
+    getRainAreaUrlFromTimestamp(1625369575346, 50),
+    'http://www.weather.gov.sg/files/rainarea/50km/v2/dpsri_70km_2021070411300000dBR.dpsri.png'
+  )
+  t.is(
+    getRainAreaUrlFromTimestamp(1625383277705, 50),
+    'http://www.weather.gov.sg/files/rainarea/50km/v2/dpsri_70km_2021070415200000dBR.dpsri.png'
+  )
+})
