05a8ee08a8998b9580af98fad2ec46476f322f8e
Maythee Anegboonlap <null@llun.me>
2021-07-05 13:17:49 +0800

    Add 240 and 480 km radius implementation

--- singapore.js
+++ singapore.js
@@ -25,7 +25,7 @@ export function getFileNameTime(timestamp, minutesFloor) {
  *
  * @param {number} timestamp
  * @param {50 | 240 | 480} areaSizeInKm
- * @returns {string}
+ * @returns {string | null}
  */
 export function getRainAreaUrlFromTimestamp(timestamp, areaSizeInKm) {
   switch (areaSizeInKm) {
@@ -35,8 +35,16 @@ export function getRainAreaUrlFromTimestamp(timestamp, areaSizeInKm) {
         5
       )}dBR.dpsri.png`
     case 240:
-      return ''
+      return `http://www.weather.gov.sg/files/rainarea/240km/dpsri_240km_${getFileNameTime(
+        timestamp,
+        15
+      )}dBR.dpsri.png`
+    case 480:
+      return `http://www.weather.gov.sg/files/rainarea/480km/dpsri_480km_${getFileNameTime(
+        timestamp,
+        30
+      )}dBR.dpsri.png`
     default:
-      return ''
+      return null
   }
 }
